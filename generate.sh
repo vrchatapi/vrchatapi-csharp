5de6758ce84867e5b4b845ce4e50fe92f5022865
Foorack <max@foorack.com>
2021-08-16 22:02:26 +0200

    fix: generate.sh from js to csharp

--- generate.sh
+++ generate.sh
@@ -4,10 +4,10 @@ openapi-generator-cli generate \
 -g csharp \
 --additional-properties=packageName=io.github.vrchatapi \
 --git-user-id=vrchatapi \
---git-repo-id=vrchatapi-javascript \
+--git-repo-id=vrchatapi-csharp \
 -o . \
--i ../specification/openapi.yaml \
---http-user-agent="vrchatapi-javascript"
+-i https://vrchatapi.github.io/specification/openapi.yaml \
+--http-user-agent="vrchatapi-csharp"
 
 # Modify package.json
 sed -i 's/Unlicense/MIT/g' ./package.json
