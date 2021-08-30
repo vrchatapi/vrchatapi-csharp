64e3082d9256f294a8b9422e00d9b5627b136528
Foorack <max@foorack.com>
2021-08-30 11:09:41 +0200

    Remove auto-generated empty unit tests

--- generate.sh
+++ generate.sh
@@ -11,8 +11,9 @@ openapi-generator-cli generate \
 -i https://vrchatapi.github.io/specification/openapi.yaml \
 --http-user-agent="vrchatapi-csharp"
 
-cp io.github.vrchatapi.csproj-template       src/io.github.vrchatapi/io.github.vrchatapi.csproj
-cp io.github.vrchatapi.Test.csproj-template  src/io.github.vrchatapi.Test/io.github.vrchatapi.Test.csproj
+rmdir src/io.github.vrchatapi.Test/
+
+cp io.github.vrchatapi.csproj-template src/io.github.vrchatapi/io.github.vrchatapi.csproj
 
 # Enable global cookie storage
 sed -i '/RestClient = new RestClient/a \\n            this.RestClient.CookieContainer = new CookieContainer();\n' ./src/io.github.vrchatapi/Client/ApiClient.cs
