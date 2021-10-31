3e2bb1e76d269880a61820bc0363b130b5b45761
Foorack <max@foorack.com>
2021-10-31 02:39:09 +0200

    ci: update specification URL for lower cache time

--- generate.sh
+++ generate.sh
@@ -10,7 +10,7 @@ rm src docs -rf
 --git-user-id=vrchatapi \
 --git-repo-id=vrchatapi-csharp \
 -o . \
--i https://vrchatapi.github.io/specification/openapi.yaml \
+-i https://raw.githubusercontent.com/vrchatapi/specification/gh-pages/openapi.yaml \
 --http-user-agent="vrchatapi-csharp"
 
 rmdir src/io.github.vrchatapi.Test/
