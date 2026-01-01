b7b88af8721845f74021b0db58bfb0156fa07581
ariesclark <ariesclark@users.noreply.github.com>
2026-01-01 22:25:52 +0000

    Upgrade .NET SDK to spec 2.20.7

--- src/VRChat.API/Client/Configuration.cs
+++ src/VRChat.API/Client/Configuration.cs
@@ -33,7 +33,7 @@ namespace VRChat.API.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "2.20.8-nightly.0";
+        public const string Version = "2.20.7";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -540,7 +540,7 @@ namespace VRChat.API.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.20.7\n";
-            report += "    SDK Package Version: 2.20.8-nightly.0\n";
+            report += "    SDK Package Version: 2.20.7\n";
 
             return report;
         }
