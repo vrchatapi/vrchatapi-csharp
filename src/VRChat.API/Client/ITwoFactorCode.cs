782844e46ee45a1a968b76a364827db714c069d1
ariesclark <ariesclark@users.noreply.github.com>
2026-09-03 06:52:07 +0000

    Upgrade .NET SDK to spec 2.20.9

--- src/VRChat.API/Client/ITwoFactorCode.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace VRChat.API.Model
-{
-    /// <summary>
-    /// An empty interface used to determine the type of two-factor code being provided.
-    /// </summary>
-    public interface ITwoFactorCode { }
-}
